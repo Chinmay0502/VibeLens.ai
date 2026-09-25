e701f657ec2dc4a044a70adc72726179b855e935
Chinmay0502 <chinamayakumarsahoo@gmai.com>
2026-09-25 15:34:01 +0530

    small change in downlaod_youtube-audio

--- backend/utils/audio_processor.py
+++ backend/utils/audio_processor.py
@@ -42,7 +42,8 @@ def download_youtube_audio(url: str) -> str:
             }
         ],
         
-        "quiet": True,
+        "quiet": False,
+        "verbose": True
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
